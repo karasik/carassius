79ae8df824677538599f7a5ef3c9216907bc503f
NoxAndry <andry.popov.nox@gmail.com>
2013-06-27 12:08:03 +0400

    merging

--- GameProject/src/server/Global.java
+++ GameProject/src/server/Global.java
@@ -3,14 +3,8 @@ package server;
 public class Global {
 	public static final int VISIBLE_DISTANCE = 8;
 	public static final int NUM_PLAYERS = 1;
-<<<<<<< HEAD
 	public static final int DELAY = 10;
 	public static final int REFRESH_PROJECTILE_TIME = 10;
 	
-=======
-	public static final int DELAY = 100;
-	public static final int REFRESH_PROJECTILE_TIME = 10;
-
->>>>>>> 06ae4f7efb26e3fb180cb85ce5f30520e03cf91c
 	public static int time = 0;
 }
